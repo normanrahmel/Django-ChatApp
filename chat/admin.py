5929e7fa861f99e5efd094cd96a82a9760ee3d1f
normanrahmel <92368615+normanrahmel@users.noreply.github.com>
2022-10-04 15:14:55 +0200

    Django Admin Config

--- chat/admin.py
+++ chat/admin.py
@@ -1,3 +1,13 @@
+from dataclasses import fields
 from django.contrib import admin
+from .models import Message
+
+
+class MessageAdmin(admin.ModelAdmin):
+    fields = ("text", "author", "receiver", "created_at")
+    list_display = ("text", "author", "receiver", "created_at")
+    search_fields = ("text",)
+
 
 # Register your models here.
+admin.site.register(Message, MessageAdmin)
